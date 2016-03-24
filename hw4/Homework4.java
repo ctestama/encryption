1e501c894e69ddefae2affe30a5e002258d8061a
ctestama <ctestama@asu.edu>
2016-03-23 20:10:23 -0700

    printing private key

--- hw4/Homework4.java
+++ hw4/Homework4.java
@@ -2,6 +2,14 @@ import java.security.cert.*;
 import java.text.*;
 import java.util.*;
 import java.io.*;
+import java.security.KeyStore;
+import javax.crypto.SecretKey;
+import javax.security.auth.callback.*;
+import java.security.PrivateKey;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import org.apache.commons.codec.binary.Base64;
 
 
 
@@ -15,7 +23,7 @@ public class Homework4 {
 
         try {
             
-
+        	//read the certificate
 			FileInputStream fis = new FileInputStream(filename);
 	 		BufferedInputStream bis = new BufferedInputStream(fis);
 
@@ -25,6 +33,27 @@ public class Homework4 {
     			Certificate cert = cf.generateCertificate(bis);
     			System.out.println(cert.toString());
  			}
+
+ 			//Read the private key
+
+ 			String path = "Raghupri.pfx";
+		    char[] pass = "raghu".toCharArray();
+
+		    KeyStore ks = KeyStore.getInstance("PKCS12");
+		    ks.load(new FileInputStream(path), pass);
+		    String alias = (String) ks.aliases().nextElement(); 
+		    PrivateKey pk = (PrivateKey) ks.getKey(alias, pass);/* returns null */
+		    //Certificate[] chain = ks.getCertificateChain(alias);/* returns null */
+		    //X509Certificate last = (X509Certificate) chain[chain.length - 1];
+
+
+		    //Key key = ks.getKey(alias, "raghu".toCharArray())
+        	byte[] b64 = Base64.encodeBase64(pk.getEncoded());
+        	System.out.println("-----BEGIN PRIVATE KEY-----");
+        	System.out.println( new String(b64 ));
+        	System.out.println("-----END PRIVATE KEY-----");
+		    //System.out.println(last.getNotBefore());
+		    //System.out.println(last.toString());
 
             // Always close files.
             bis.close();         
@@ -43,6 +72,15 @@ public class Homework4 {
         }
         catch(CertificateException ex) {
         	System.out.println("Certificate Exception");
+        }
+        catch(KeyStoreException ex) {
+        	System.out.println("Keystore Exception");
+        }
+        catch(NoSuchAlgorithmException ex) {
+        	System.out.println("NoSuchAlgorithmException");
+        }
+        catch(UnrecoverableKeyException ex) {
+        	System.out.println("UnrecoverableKeyException");
         }
 	}
 
